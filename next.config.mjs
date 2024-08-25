05d4f8e2295052315dcc5bae1492327b4d975b71
nagatamasato <hgtm.kabocha@gmail.com>
2024-08-25 21:41:31 +0900

    Chapter 10 complete

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    esmExternals: {
+      ppr: 'incremental',
+    }
+  },
+};
 
 export default nextConfig;
